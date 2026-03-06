5951de67617a812a73a3024c1089dda082618081
Lucas Velasco <158297160+Wisineano@users.noreply.github.com>
2026-03-05 21:08:53 -0300

    Add files via upload
    ASI USAMOS EL COSO DE PIXELES PORFAVOR

---
+++ Clase 8/Calibracion_pixeles_bueno.py
@@ -0,0 +1,248 @@
+import imageio.v2 as imageio
+import matplotlib.pyplot as plt 
+from pathlib import Path
+import numpy as np
+from matplotlib.colors import LogNorm
+from matplotlib.colors import SymLogNorm
+import os
+from scipy.signal import find_peaks
+from scipy.ndimage import rotate
+
+ROOT = Path(r"C:\Users\tomas\Desktop\Laboratorio-4-cobelli-main\Clase 8\young _2\aluminium_")
+extensiones_validas = ('.tif', '.tiff', '.png', '.jpg', '.jpeg')
+archivos = [f for f in os.listdir(ROOT) if f.lower().endswith(extensiones_validas)]
+# archivos.sort()  # opcional, para orden alfabético
+    
+images = []
+for archivo in archivos:
+    ruta_completa = ROOT / archivo
+    try:
+        imagen = imageio.imread(ruta_completa)
+        images.append(imagen)
+    except Exception as e:
+        print(f"Error al leer {archivo}: {e}")
+
+if not images:
+    print("No se encontraron imágenes.")
+    exit()
+
+imagen = images[0]
+
+center_y = 890                                              #LA PRIMERA PARTE DEL CORCHETE SON LAS FILAS, EJE Y, la segunda las columnas EJE X
+center_x = 1660
+offset  = 600
+
+matriz = imagen[center_y - offset : center_y + offset,
+                center_x - offset : center_x + offset,
+                2].astype(float)
+
+center_y2 = 750
+center_x2 = 2500       
+offset2  = 600
+
+matriz2 = imagen[center_y2 - offset2 : center_y2 + offset2,
+                center_x2 - offset2 : center_x2 + offset2,
+                2].astype(float)
+
+angulo_correccion = 1.2
+angulo_correccion2 = 0.7
+matriz_rotada = rotate(matriz, angulo_correccion, reshape=False, order=3)
+matriz2_rotada = rotate(matriz2, angulo_correccion2, reshape=False, order=3)
+
+plt.figure()
+plt.imshow(imagen)
+plt.show()
+
+plt.figure()
+plt.imshow(matriz)
+plt.show()
+
+plt.figure()
+plt.imshow(matriz_rotada)
+plt.show()
+
+plt.figure()
+plt.imshow(matriz2)
+plt.show()
+
+plt.figure()
+plt.imshow(matriz2_rotada)
+plt.show()
+# %%
+
+#no uso al final lo de fourier
+f = np.fft.fft2(matriz)
+fshift = np.fft.fftshift(f)
+fshift_abs = np.abs(fshift)
+espectro_log = np.log10(1 + fshift_abs)
+
+f2 = np.fft.fft2(matriz2)
+fshift2 = np.fft.fftshift(f2)
+fshift_abs2 = np.abs(fshift2)
+espectro_log2 = np.log10(1 + fshift_abs2)
+
+plt.figure()
+plt.imshow(espectro_log)
+plt.show()
+
+row=600
+perfil_espectro_log = espectro_log[row, :]
+
+row2=610
+perfil_espectro_log2 = espectro_log2[row, :]
+
+plt.figure()
+plt.plot(perfil_espectro_log)
+plt.show()
+
+plt.figure()
+plt.plot(perfil_espectro_log2)
+
+plt.show()
+
+#esto si lo uso
+perfil_matriz=matriz[row, :]
+plt.figure()
+plt.plot(perfil_matriz)
+plt.title('perfil_matriz')
+plt.show()
+
+perfil_matriz2=matriz2[row2, :]
+plt.figure()
+plt.plot(perfil_matriz2)
+plt.title('perfil_matriz2')
+plt.show()
+
+
+# %%
+
+peaks, properties = find_peaks(
+    -perfil_matriz,
+    prominence = 20,  # <-- AJUSTA ESTE VALOR (ej. 5, 10 o 20) según la altura de tus valles
+    distance = 20,      # <-- Evita que tome picos muy pegados entre sí
+    width       = None                              
+)
+
+
+if len(peaks) >= 2:
+    pasos = np.diff(peaks.astype(float))          # diferencias en píxeles (float para precisión)
+    paso_promedio = np.mean(pasos)
+    paso_std      = np.std(pasos)
+    paso_min      = np.min(pasos)
+    paso_max      = np.max(pasos)
+    
+    print(f"✅ Paso promedio entre picos consecutivos: {paso_promedio:.2f} ± {paso_std:.2f} píxeles")
+    print(f"   Mínimo paso: {paso_min:.1f} px  |  Máximo paso: {paso_max:.1f} px")
+    
+    centro_recorte = row                     
+    idx_central = np.argmin(np.abs(peaks - centro_recorte))
+    pico_central = peaks[idx_central]
+    
+    print(f"   Pico central (m=0) en fila: {pico_central} (distancia al centro del recorte: {abs(pico_central - centro_recorte)} px)")
+    
+    # Asignar órdenes m (izquierda = negativos, derecha = positivos)
+    ordenes = np.arange(-idx_central, len(peaks) - idx_central)
+    # ====================== GRÁFICO DEL PASO (linealidad del patrón) ======================
+    plt.figure(figsize=(12, 6))
+    
+    # Posición vs Orden (debe ser una recta perfecta en difracción ideal)
+    plt.subplot(1, 2, 1)
+    plt.plot(ordenes, peaks, 'o-', color='tab:red', markersize=8, linewidth=2.5, label='Posiciones medidas')
+    
+    # Ajuste lineal (pendiente = paso promedio)
+    coef,cov = np.polyfit(ordenes, peaks, 1 ,cov=True)
+    paso_fit = coef[0]
+    error_coef = np.sqrt(np.diag(cov))
+    plt.plot(ordenes, coef[0]*ordenes + coef[1], '--', color='black', label=f'Ajuste lineal\npaso = {paso_fit:.2f} +- {error_coef[0]:.2f} px/mm ')
+    
+    plt.xlabel('milimetros de paso')
+    plt.ylabel('Posición en la imagen (píxeles)')
+    plt.grid(True, alpha=0.3)
+    plt.legend()
+    
+    plt.subplot(1, 2, 2)
+    plt.plot(matriz[row, :],".-" , label='Original', linewidth=1.5, alpha = 0.1)
+    plt.plot(perfil_matriz, color='tab:blue', linewidth=2.5, label='Perfil filtrado')
+    plt.plot(peaks, perfil_matriz[peaks], "x", markersize=12, color='red', label='Picos')
+    plt.show()
+else:
+    print("no se encontraron picos")
+
+# paso_fit = pixeles / mm
+
+
+dist_real = 1  # mm (CAMBIAR por referencia real)
+
+# %%
+peaks, properties = find_peaks(
+    -perfil_matriz2,
+    prominence = 20,  # <-- AJUSTA ESTE VALOR (ej. 5, 10 o 20) según la altura de tus valles
+    distance = 20,      # <-- Evita que tome picos muy pegados entre sí
+    width       = None                              
+)
+
+
+if len(peaks) >= 2:
+    pasos = np.diff(peaks.astype(float))          # diferencias en píxeles (float para precisión)
+    paso_promedio = np.mean(pasos)
+    paso_std      = np.std(pasos)
+    paso_min      = np.min(pasos)
+    paso_max      = np.max(pasos)
+    
+    print(f"✅ Paso promedio entre picos consecutivos: {paso_promedio:.2f} ± {paso_std:.2f} píxeles")
+    print(f"   Mínimo paso: {paso_min:.1f} px  |  Máximo paso: {paso_max:.1f} px")
+    
+    centro_recorte = row2                     
+    idx_central = np.argmin(np.abs(peaks - centro_recorte))
+    pico_central = peaks[idx_central]
+    
+    print(f"   Pico central (m=0) en fila: {pico_central} (distancia al centro del recorte: {abs(pico_central - centro_recorte)} px)")
+    
+    # Asignar órdenes m (izquierda = negativos, derecha = positivos)
+    ordenes = np.arange(-idx_central, len(peaks) - idx_central)
+    # ====================== GRÁFICO DEL PASO (linealidad del patrón) ======================
+    plt.figure(figsize=(12, 6))
+    
+    # Posición vs Orden (debe ser una recta perfecta en difracción ideal)
+    plt.subplot(1, 2, 1)
+    plt.plot(ordenes, peaks, 'o-', color='tab:red', markersize=8, linewidth=2.5, label='Posiciones medidas')
+    
+    # Ajuste lineal (pendiente = paso promedio)
+    coef,cov = np.polyfit(ordenes, peaks, 1 ,cov=True)
+    paso_fit = coef[0]
+    error_coef = np.sqrt(np.diag(cov))
+    plt.plot(ordenes, coef[0]*ordenes + coef[1], '--', color='black', label=f'Ajuste lineal\npaso = {paso_fit:.2f} +- {error_coef[0]:.2f} px/mm ')
+    
+    plt.xlabel('milimetros de paso')
+    plt.ylabel('Posición en la imagen (píxeles)')
+    plt.grid(True, alpha=0.3)
+    plt.legend()
+    
+    plt.subplot(1, 2, 2)
+    plt.plot(matriz[row, :],".-" , label='Original', linewidth=1.5, alpha = 0.1)
+    plt.plot(perfil_matriz2, color='tab:blue', linewidth=2.5, label='Perfil filtrado')
+    plt.plot(peaks, perfil_matriz2[peaks], "x", markersize=12, color='red', label='Picos')
+    plt.show()
+else:
+    print("no se encontraron picos")
+
+# paso_fit = pixeles / mm
+
+
+dist_real = 1  # mm (CAMBIAR por referencia real)
+
+# %%
+#para matriz 1
+escala=23.86 #px/mm
+err_escala=0.02 #px/mm
+escala2=24.72      #este es el bueno porque esta mas perpendicular la foto a esa posicion
+err_escala2=0.04 #px/mm
+err_sist=escala2-escala
+
+
+#USAR ESTE ERROR
+err_escala2_total=0.04+err_sist  #px/mm
+print(err_escala2_total)
+
+
+
